ca10a358b9e994802dffc5628962107d06fd421f
Carie Frantz <cariefrantz@weber.edu>
2022-09-30 16:43:40 -0600

    Added light, pressure plots

--- timeseries_plots.py
+++ timeseries_plots.py
@@ -34,12 +34,109 @@ years = mdates.YearLocator()
 months = mdates.MonthLocator()
 years_fmt = mdates.DateFormatter('%Y')
 
-
-####################
-# FUNCTIONS
-####################
-
+# Plots to generate
+plots = {
+    'elev'  : {
+        'ytitle'    : 'Lake elevation (ft asl)',
+        'datasets'  : {
+            'elevation_Saltair' : {
+                'file'  : 'elevation_Saltair',
+                'ycol'  : '14n'
+                }
+            }
+        },
+    'temp'  : {
+        'ytitle'    : 'Temperature (' + chr(176) + 'C)',
+        'datasets'  : {
+            'Site A Pendant'   : {
+                'file'  : 'site_A_pend',
+                'ycol'  : 'T_C'
+                },
+            'Site A Button A (top)'     : {
+                'file'  : 'site_A_butt_A',
+                'ycol'  : 'T_C'
+                },
+            'Site B Pendant'   : {
+                'file'  : 'site_B_pend',
+                'ycol'  : 'T_C'
+                },
+            'Site B Button B (top)'     : {
+                'file'  : 'site_B_butt_B',
+                'ycol'  : 'T_C'
+                },
+            'Site B Button C (side)'    : {
+                'file'  : 'site_B_butt_C',
+                'ycol'  : 'T_C'
+                }
+            }
+        },
+    'light' : {
+        'ytitle'    : 'Light intensity (lumen/ft2)',
+        'datasets'  : {
+            'Site A Button A (top)'     : {
+                'file'  : 'site_A_butt_A',
+                'ycol'  : 'Lux_lum_ft2'
+                },
+            'Site B Button B (top)'     : {
+                'file'  : 'site_B_butt_B',
+                'ycol'  : 'Lux_lum_ft2'
+                },
+            'Site B Button C (side)'     : {
+                'file'  : 'site_B_butt_C',
+                'ycol'  : 'Lux_lum_ft2'
+                }
+            }
+        },
+    'pressure'  : {
+        'ytitle'    : 'Absolute pressure (in Hg)',
+        'datasets'  : {
+            'Site A Pendant'   : {
+                'file'  : 'site_A_pend',
+                'ycol'  : 'P_abs'
+                },
+            'Site B Pendant'   : {
+                'file'  : 'site_B_pend',
+                'ycol'  : 'P_abs'
+                },
+            }
+        }
+    }
     
+# Datasets to draw from
+files = {
+    'elevation_Saltair'   : {
+        'title'     : 'Elevation at Saltair (USGS)',
+        'xcol'      : '20d',
+        'datefmt'   : '%m/%d/%Y'
+        },
+    'site_A_pend'   : {
+        'title'     : 'Site A Pendant',
+        'xcol'      : 'Datetime',
+        'datefmt'   : '%m/%d/%Y %H:%M'
+        },
+    'site_A_butt_A' : {
+        'title'     : 'Site A Button A (top)',
+        'xcol'      : 'Datetime',
+        'datefmt'   : '%m/%d/%Y %H:%M'
+        },
+    'site_B_pend'   : {
+        'title'     : 'Site B Pendant',
+        'xcol'      : 'Datetime',
+        'datefmt'   : '%m/%d/%Y %H:%M'
+        },
+    'site_B_butt_B' : {
+        'title'     : 'Site B Button B (top)',
+        'xcol'      : 'Datetime',
+        'datefmt'   : '%m/%d/%Y %H:%M'
+        },
+    'site_B_butt_C' : {
+        'title'     : 'Site B Button C (side)',
+        'xcol'      : 'Datetime',
+        'ycol'      : 'T_C',
+        'datefmt'   : '%m/%d/%Y %H:%M'
+        }
+    }
+   
     
     
 #%%
@@ -47,67 +144,18 @@ years_fmt = mdates.DateFormatter('%Y')
 # MAIN FUNCTION
 ####################
 if __name__ == '__main__': 
-    ###############
-    # Variables
-    
-    # Plots to generate
-    plots = {
-        'elev'  : {
-            'datasets'  : ['elevation_Saltair'],
-            'ytitle'    : 'Lake elevation (ft asl)'
-            },
-        'temp'  : {
-            'datasets'  : ['temp_A_pend', 'temp_A_butt_A', 'temp_B_pend',
-                           'temp_B_butt_B', 'temp_B_butt_C'],
-            'ytitle'    : 'Temperature (' + chr(176) + 'C)'
-            }
-        }
-        
-    # Datasets to draw from
-    datasets = {
-        'elevation_Saltair'   : {
-            'title'     : 'Elevation at Saltair (USGS)',
-            'xcol'      : '20d',
-            'ycol'      : '14n',
-            'datefmt'   : '%m/%d/%Y'
-            },
-        'temp_A_pend'   : {
-            'title'     : 'Site A Pendant',
-            'xcol'      : 'Datetime',
-            'ycol'      : 'T_C',
-            'datefmt'   : '%m/%d/%Y %H:%M'
-            },
-        'temp_A_butt_A' : {
-            'title'     : 'Site A Button A (top)',
-            'xcol'      : 'Datetime',
-            'ycol'      : 'T_C',
-            'datefmt'   : '%m/%d/%Y %H:%M'
-            },
-        'temp_B_pend'   : {
-            'title'     : 'Site B Pendant',
-            'xcol'      : 'Datetime',
-            'ycol'      : 'T_C',
-            'datefmt'   : '%m/%d/%Y %H:%M'
-            },
-        'temp_B_butt_B' : {
-            'title'     : 'Site B Button B (top)',
-            'xcol'      : 'Datetime',
-            'ycol'      : 'T_C',
-            'datefmt'   : '%m/%d/%Y %H:%M'
-            },
-        'temp_B_butt_C' : {
-            'title'     : 'Site B Button C (side)',
-            'xcol'      : 'Datetime',
-            'ycol'      : 'T_C',
-            'datefmt'   : '%m/%d/%Y %H:%M'
-            },
-        }
-    
-    ###############
-    # Plotting code
-    
+      
+    # Load in files
     dirpath = os.getcwd()
+    for file in files:
+        # Load in the file
+        filename, dirpath, data = ResearchModules.fileGet(
+            'Select file with data for ' + files[file]['title'],
+            directory=dirpath)
+        files[file]['data'] = data
+        
     
+    # Plot figures
     for plot in plots:
         # Create a new figure
         fig, ax = plt.subplots(figsize=plotsize_default)
@@ -115,20 +163,19 @@ if __name__ == '__main__':
     
         # Plot each line
         for var in plots[plot]['datasets']:
-            # Load in the file
-            filename, dirpath, data = ResearchModules.fileGet(
-                'Select file with data for ' + datasets[var]['title'],
-                directory=dirpath)
+            # Get the data
+            file = plots[plot]['datasets'][var]['file']
                   
             # Plot the data
             ResearchModules.plotData(
-                data, datasets[var]['xcol'], datasets[var]['ycol'], 'Date',
-                plots[plot]['ytitle'], datasets[var]['title'], ax=ax,
-                plotsize=plotsize_default, smooth=True, xtype='datetime',
-                datefmt=datasets[var]['datefmt'])
+                files[file]['data'], files[file]['xcol'],
+                plots[plot]['datasets'][var]['ycol'], 'Date',
+                plots[plot]['ytitle'], var, ax=ax, plotsize=plotsize_default,
+                smooth=True, xtype='datetime',
+                datefmt=files[file]['datefmt'])
             
             # Add to legend
-            legend.append(datasets[var]['title'])
+            legend.append(var)
             
         # Add the legend
         if len(legend)>1:
